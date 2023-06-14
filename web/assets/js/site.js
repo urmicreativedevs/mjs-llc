2d9f5586949315af12d106fa288c0d7fca01543e
Raja Afrika <raja@marijuanasoftwarellc.com>
2023-06-14 00:14:36 +0000

    Removed site.js from project.

--- web/assets/js/site.js
+++
@@ -1,8 +0,0 @@
-function ensureHTTPS() {
-    if (location.protocol !== 'https:') {
-      location.replace('https://' + location.hostname + location.pathname + location.search);
-    }
-  }
-  
-  window.onload = ensureHTTPS;
-  
